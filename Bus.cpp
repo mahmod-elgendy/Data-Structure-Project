67dc8940ca7b4a6bb3a48aea5b614358b3c934ec
AbeerAmin <130995589+AbeerAmin@users.noreply.github.com>
2023-12-31 20:38:30 +0200

    Update Bus.cpp

--- Bus.cpp
+++ Bus.cpp
@@ -1,4 +1,242 @@
-/*
+#include <iostream>
+using namespace std;
+#include <string>
+#include <queue>
+
+
+
+class passenger {
+private:
+    string passStart;
+    string passEnd;
+    string passType;
+
+public:
+    string arrivalTime;
+    int passGetOn;
+    int passGetOff;
+    int passId;
+    int passDestination;
+    int NPs;
+    int SPs;
+    int WPs;
+    passenger(string arrivalTime, string passStart, string passEnd, int passGetOn, int passGetOff, string passType , int passId, int passDestination) {
+        arrivalTime = arrivalTime;
+        passStart = passStart;
+        passEnd = passEnd;
+        passGetOn = passGetOn;
+        passGetOff = passGetOff;
+        passType = passType;
+        passId = passId;
+        passDestination = passDestination;
+    }
+    passenger(int id, int dest) : passId(id), passDestination(dest) {}
+
+    int getNP() {
+        return NPs;
+    }
+
+    int getSP() {
+        return SPs;
+    }
+
+    int getWP() {
+        return WPs;
+    }
+
+    void setArrivalTime(string time) {
+        arrivalTime = time;
+    }
+
+    string getArrivalTime() {
+        return arrivalTime;
+    }
+
+    void setPassStart(string start) {
+        passStart = start;
+    }
+
+    string getPassStart() {
+        return passStart;
+    }
+
+    void setPassEnd(string end) {
+        passEnd = end;
+    }
+
+    string getPassEnd() {
+        return passEnd;
+    }
+    void setPassGetOn(int getOn) {
+        passGetOn = getOn;
+    }
+
+    int getPassGetOn() {
+        return passGetOn;
+    }
+
+
+    void setPassGetOff(int getOff) {
+        passGetOff = getOff;
+    }
+
+    int getPassGetOff() {
+        return passGetOff;
+    }
+
+    void setpassDestination(int passDestination) {
+        passDestination = passDestination;
+    }
+
+    int getpassDestination() {
+        return passDestination;
+    }
+
+    void setPassType(string type) {
+        passType = type;
+    }
+
+    string getPassType() {
+        return passType;
+    }
+
+    void modifyArrivalTime(int time) {
+        arrivalTime = time;
+    }
+    void modifyPassGetOn(int getOn) {
+        passGetOn = getOn;
+    }
+
+    void modifyPassGetOff(int getOff) {
+        passGetOff = getOff;
+    }
+
+    void modifyPassType(string type) {
+        passType = type;
+    }
+};
+
+template <typename V>
+struct Node {
+    V data;
+    Node<V>* next;
+};
+template <typename T>
+class Queue {
+private:
+    Node<T>* rear;
+    int MaxCapacity;
+
+public:
+    Queue(int capacity) : MaxCapacity(capacity), rear(nullptr) {}
+    ~Queue() {
+        while (!isEmpty()) {
+            delete dequeue();
+        }
+    }
+
+    bool isEmpty() const {
+        return rear == nullptr;
+    }
+
+    void enqueue(T passenger) {
+        Node<T>* newNode = new Node<T>(passenger);
+        if (isEmpty()) {
+            return nullptr;
+        }
+        else {
+            Node<T>* front = rear->next;
+            if (front == rear) {
+                rear = nullptr;
+            }
+            else {
+                rear->next = front->next;
+            }
+            front->next = nullptr;
+            return front;
+        }
+    }
+
+    Node<T>* dequeue() {
+        if (isEmpty()) {
+            return nullptr;
+        }
+        else {
+            Node<T>* front = rear->next;
+            if (front == rear) {
+                rear = nullptr;
+            }
+            else {
+                rear->next = front->next;
+            }
+            front->next = nullptr;
+            return front;
+        }
+    }
+
+    Node<T>* dequeueSpecificElement(T passenger) {
+        if (isEmpty()) {
+            return nullptr;
+        }
+        else {
+            Node<T>* current = rear->next;
+            Node<T>* previous = nullptr;
+
+            while (current != nullptr) {
+                if (current->data == passenger) {
+                    if (current == rear->next) {
+                        rear->next = current->next;
+                        if (current == rear) {
+                            rear = nullptr;
+                        }
+                    }
+                    else {
+                        previous->next = current->next;
+                        if (current == rear) {
+                            rear = previous;
+                        }
+                    }
+                    current->next = nullptr;
+                    return current;
+                }
+                previous = current;
+                current = current->next;
+            }
+
+            return nullptr;
+        }
+    }
+
+    bool isFull() const {
+        return (MaxCapacity > 0) && (size() >= MaxCapacity);
+    }
+
+    int size() const {
+        if (isEmpty()) {
+            return 0;
+        }
+
+        int count = 1;
+        Node<T>* temp = rear->next;
+        while (temp != rear) {
+            count++;
+            temp = temp->next;
+        }
+
+        return count;
+    }
+
+    T peek() const {
+        if (isEmpty()) {
+            return T();
+        }
+        else {
+            Node<T>* front = rear->next;
+            return front->data;
+        }
+    }
+};
+
 template <typename T>
 struct PriNode {
     T data;
@@ -58,7 +296,23 @@ public:
         }
         throw out_of_range("PriorityQueue is empty");
     }
-};*/
+};
+
+struct ComparePassenger {
+    bool operator()(const passenger* p1, const passenger* p2) {
+        int stationDest = 10; // Example station destination
+
+        int p1DestDiff = std::abs(p1->passDestination - stationDest);
+        int p2DestDiff = std::abs(p2->passDestination - stationDest);
+
+        if (p1DestDiff != p2DestDiff) {
+            return p1DestDiff > p2DestDiff;
+        }
+        else {
+            return p1->passId > p2->passId;
+        }
+    }
+};
 
 
 class Bus {
@@ -69,7 +323,6 @@ private:
     int maintenance_duration;
     int JourneyCount;
     //int AvailableSeats; //if or while
-    //Queue<Passenger*> passengers;
     int currentPassengerCount;
     int currentStation;
     int moveTime;
@@ -91,16 +344,6 @@ public:
         moveTime = moveTime;
         getOffTime = getOffTime;
     }
-
-
-
-    //_________
-    /*Bus::~Bus() {
-    while (!passengers.isEmpty()) {
-        delete passengers.dequeue()->data;
-    }
-}*/
-//__________
 
     char getbustype(){
         return bustype;
@@ -172,9 +415,44 @@ public:
     void setgetOffTime(char getoffTime) {
         getoffTime = getoffTime;
     }
-    /*bool Bus::hasAvailableSeats() const {
+    /*
+    bool Bus::hasAvailableSeats() const {
         return passengers.size() < capacity;
     }*/
 
 
 };
+
+
+class Bus1 {
+private:
+    priority_queue<passenger*, vector<passenger*>, ComparePassenger> passengerList;
+
+public:
+    void addPassenger(passenger* passenger) {
+        passengerList.push(passenger);
+    }
+
+    void printPassengers() {
+        while (!passengerList.empty()) {
+            passenger* p = passengerList.top();
+            cout << "Passenger ID: " << p->passId << ", Destination: " << p->passDestination << endl;
+            passengerList.pop();
+            delete p;
+        }
+    }
+};
+
+int main() {
+    Bus1 bus;
+    bus.addPassenger(new passenger(1, 5));
+    bus.addPassenger(new passenger(2, 3));
+    bus.addPassenger(new passenger(3, 7));
+    bus.addPassenger(new passenger(4, 6));
+    bus.addPassenger(new passenger(5, 4));
+
+    cout << "Passengers in the bus:" << endl;
+    bus.printPassengers();
+
+    return 0;
+}
