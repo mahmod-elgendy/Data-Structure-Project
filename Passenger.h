3332ab03a35fd4ba7fc8d47d1a823a314de943b5
AbeerAmin <130995589+AbeerAmin@users.noreply.github.com>
2024-01-05 23:54:15 +0200

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -1,1 +1,42 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
+#include <string>
 
+class passenger {
+private:
+	string arrivalTime;
+	string passStart;
+	string passEnd;
+	int passengerID;
+	int passGetOn;
+	int passGetOff;
+	string passType;
+
+public:
+	passenger(string arrivalTime, string passStart, string passEnd, int passengerID, int passGetOn, int passGetOff, string passType);
+
+	void setArrivalTime(string time);
+	     string getArrivalTime();
+
+	void setPassStart(string start);
+	     string getPassStart();
+
+	void setPassEnd(string end);
+	     string getPassEnd();
+
+	void setPassengerID(int id);
+	     int getPassengerID();
+
+	void setPassGetOn(int getOn);
+	     int getPassGetOn();
+
+	void setPassGetOff(int getOff);
+     	int getPassGetOff();
+
+	void setPassType(string type);
+	    string getPassType();
+
+	bool operator==(const passenger& Passenger2) const;
+};
+
+#endif
