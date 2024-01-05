83ea7f6708b55eecf0ca92a7aba6b0139509a3a3
AbeerAmin <130995589+AbeerAmin@users.noreply.github.com>
2024-01-05 23:59:59 +0200

    Update Passenger.cpp

--- Passenger.cpp
+++ Passenger.cpp
@@ -1,108 +1,72 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "passenger.h"
+
+passenger::passenger(string arrivalTime, string passStart, string passEnd, int passengerID, int passGetOn, int passGetOff, string passType) {
+
+	arrivalTime = arrivalTime;
+	passStart = passStart;
+	passEnd = passEnd;
+	passengerID = passengerID;
+	passGetOn = passGetOn;
+	passGetOff = passGetOff;
+	passType = passType;
+}
+
+void setArrivalTime(string time) {
+	arrivalTime = time;
+}
+
+string getArrivalTime() {
+	return arrivalTime;
+}
+
+void setPassStart(string start) {
+	passStart = start;
+}
+
+string getPassStart() {
+	return passStart;
+}
+
+void setPassEnd(string end) {
+	passEnd = end;
+}
+
+string getPassEnd() {
+	return passEnd;
+}
+
+void setPassengerID(int id) {
+	passengerID = id;
+}
+
+int getPassengerID() {
+	return passengerID;
+}
+
+void setPassGetOn(int getOn) {
+	passGetOn = getOn;
+}
+
+int getPassGetOn() {
+	return passGetOn;
+}
 
 
-class passenger {
-private:
-	string passStart;
-	string passEnd;
-	string passType;
+void setPassGetOff(int getOff) {
+	passGetOff = getOff;
+}
 
-public:
-    struct Passenger {
-        int id;
-        int startStation;
-        int endStation;
-    };
-	string arrivalTime;
-	int passengerID;
-	int passGetOn;
-	int passGetOff;
+int getPassGetOff() {
+	return passGetOff;
+}
 
-	passenger(string arrivalTime, string passStart, string passEnd, int passengerID, int passGetOn, int passGetOff, string passType) {
-		arrivalTime = arrivalTime;
-		passStart = passStart;
-		passEnd = passEnd;
-		passengerID = passengerID;
-		passGetOn = passGetOn;
-		passGetOff = passGetOff;
-		passType = passType;
-	}
-	void setArrivalTime(string time) {
-		arrivalTime = time;
-	}
+void setPassType(string type) {
+	passType = type;
+}
 
-	string getArrivalTime() {
-		return arrivalTime;
-	}
-
-	void setPassStart(string start) {
-		passStart = start;
-	}
-
-	string getPassStart() {
-		return passStart;
-	}
-
-	void setPassEnd(string end) {
-		passEnd = end;
-	}
-
-	string getPassEnd() {
-		return passEnd;
-	}
-
-	void setPassengerID(int id) {
-		passengerID = id;
-	}
-
-	int getPassengerID() {
-		return passengerID;
-	}
-
-	void setPassGetOn(int getOn) {
-		passGetOn = getOn;
-	}
-
-	int getPassGetOn() {
-		return passGetOn;
-	}
-
-
-	void setPassGetOff(int getOff) {
-		passGetOff = getOff;
-	}
-
-	int getPassGetOff() {
-		return passGetOff;
-	}
-
-	void setPassType(string type) {
-		passType = type;
-	}
-
-	string getPassType() {
-		return passType;
-	}
-
-	void modifyArrivalTime(int time) {
-		arrivalTime = time;
-	}
-
-	void modifyPassengerID(int id) {
-		passengerID = id;
-	}
-
-	void modifyPassGetOn(int getOn) {
-		passGetOn = getOn;
-	}
-
-	void modifyPassGetOff(int getOff) {
-		passGetOff = getOff;
-	}
-
-	void modifyPassType(string type) {
-		passType = type;
-	}
-};
+string getPassType() {
+	return passType;
+}
+bool operator==(const passenger& Passenger2) const {
+	return (passengerID == Passenger2.passengerID);
+}
