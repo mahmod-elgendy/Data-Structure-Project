cc8f70d482862715cbf85fd659a426c7e0e3395e
ORIO68 <120188073+ORIO68@users.noreply.github.com>
2024-01-09 00:37:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
+#include<iostream>
+#include "Company.h"
+#include <string>
+using namespace std;
 
+int main()
+{
+    Company myCompany;
+    string fileName = myCompany.getInputFile();
+    myCompany.OutputData();
+};
