9756a5c96ab34188e10d27ca6f5bd29c48858a73
ORIO68 <120188073+ORIO68@users.noreply.github.com>
2024-01-09 23:40:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
-#include "Company.h"
+#include "Company.cpp"
+#include "Bus.cpp"
+#include "Passenger.cpp"
+#include"UI.cpp"
 #include <string>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
     Company myCompany;
-    string fileName = myCompany.getInputFile();
-    myCompany.OutputData();
+    UI myUI;
+
+    
+    int mode = myUI.GetModeType();
+    std::ofstream outputFile("Output_file.txt");
+
+    if (mode == 2)
+    {
+        string fileName = myCompany.getInputFile();
+        myCompany.OutputData();
+    }
+    // if (mode == 1)
+    // {
+    //     myUI.printCurrentTime();
+    //     myUI.passengers_at_station();
+    //     myUI.In_Check_Up_Buses();
+    //     myUI.finished_passengers();
+    // }
+    return 0;
 };
+
