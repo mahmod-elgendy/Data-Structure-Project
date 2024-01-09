4fa7aef743550c9b95406700842ca111938718fd
ORIO68 <120188073+ORIO68@users.noreply.github.com>
2024-01-10 00:51:20 +0200

    Update UI.h

--- UI.h
+++ UI.h
@@ -1,1 +1,16 @@
+#ifndef UI_H
+#define UI_H
 
+#include <iostream>
+#include <queue>
+
+class UI {
+public:
+    int GetModeType();
+    void printCurrentTime(int hour, int minute);
+    void passengers_at_station(const std::queue<int>& WaitingPassengers);
+    void In_Check_Up_Buses(const std::queue<int>& InCheckUpbusses);
+    void finished_passengers(const std::queue<int>& FinishedPassengers);
+};
+
+#endif // UI_H
