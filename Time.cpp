729ac8a0feb8d5f7d9016940d8fba0e5bbb50c99
ORIO68 <120188073+ORIO68@users.noreply.github.com>
2024-01-10 06:39:38 +0200

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,1 +1,34 @@
+#include "Time.h"
+#include <iostream>
 
+
+Time::Time(int hour, int minute) : currentHour(hour), currentMinute(minute) {}
+
+
+void Time::addMinute(int minutes) 
+{
+    currentMinute += minutes;
+    update();  
+}
+
+
+void Time::addHour(int hours) 
+{
+    currentHour += hours;
+    update();  
+}
+
+
+void Time::update() 
+{
+    if (currentMinute >= 60) {
+        currentHour += currentMinute / 60;
+        currentMinute %= 60;
+    }
+}
+
+
+void Time::displayTime() const 
+{
+    std::cout << "Current Time (Hour:Min): " << currentHour << ":" << currentMinute << std::endl;
+}
